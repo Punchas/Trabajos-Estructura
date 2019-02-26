29675a2a604e26ded0677a3935014a744d410058
Diego Sanchez <43828936+Punchas@users.noreply.github.com>
2019-02-26 17:04:47 -0600

    Add files via upload

---
+++ funciones 2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void
+
+int main()
+{
+	
+	printf("Escoja una opcion:\n1. Area del cuadrado\n2. Area de un rectangulo\n3. Area de un circulo\n4. Area de un triangulo\n5. Salir")
+	while(scanf("%d", &x)&&!=5)
+	{
+		switch(x)
+		{
+			case 1:
+				{
+					printf("Dar valor de la base")
+					scanf("%d", &i)
+					static void
+				}
+		}
+	}
+}
