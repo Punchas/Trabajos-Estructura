40ca92b67ffd38eaa3b68952090334852d2faa87
Diego Sanchez <43828936+Punchas@users.noreply.github.com>
2019-03-14 13:37:51 -0600

    Add files via upload

--- InsertSort.cpp
+++ InsertSort.cpp
@@ -1,13 +1,20 @@
 /* 	PROGRAMA HECHO POR: DIEGO SANCHEZ REBOLLO 
 	MATRICULA: 24500608
 	ID: 3361683
-	PROGRAMA QUE HACE ORDENAMIENTO DE NUMEROS CON ALGORITMO QUICKSORT */
+	PROGRAMA QUE HACE ORDENAMIENTO DE NUMEROS CON ALGORITMO INSERTSORT */
 #include <stdio.h>
+#include <time.h>
+
 int main()
 {
+ 	time_t t;
+  	struct tm *tm;
+ 	char fechayhora[100];
+ 	
 	int arr[100], actual=0, j, i, n;
 	printf("Ingrese numero de datos a ingresar\n");
 	scanf("%d", &n);
+	printf("------------------\n");
 	for(i=0; i<n; i++)
 	scanf("%d", &arr[i]);
     for (i=1; i<n; i++) 
@@ -21,9 +28,14 @@ int main()
     	}
         arr[j+1]=actual;
     }
+    printf("Los numeros ordenados son: \n");
     for(i=0; i<n; i++)
-	printf("%d", arr[i]);
+	printf("%d ", arr[i]);
+	printf("\n-------------------\n");
+	printf("PROGRAMA HECHO POR: DIEGO SANCHEZ REBOLLO | MATRICULA: 24500608 | ID: 3361683\n");
+	t=time(NULL);
+ 	tm=localtime(&t);
+  	printf ("Hoy es: %02d/%02d/%d y la hora es: %02d:%02d:%02d\n", tm->tm_mday, tm->tm_mon, 1900+tm->tm_year, tm->tm_hour, tm->tm_min, tm->tm_sec);
 	return 0;
 }
-
 
